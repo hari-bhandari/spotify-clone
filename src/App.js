9e713d342c6790aa51ee4493c5e159046c406aac
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-10-16 09:35:51 +0100

    added login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Login from "./components/auth/Login";
 
 function App() {
   return (
     <div className="App">
+      <Login/>
     </div>
   );
 }
