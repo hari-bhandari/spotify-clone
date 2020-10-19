7fa69d7664d6253213008d508c8a70e3f2e0a83a
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-10-19 19:01:50 +0100

    added playback function on footer

--- src/components/Player/Footer/Footer.js
+++ src/components/Player/Footer/Footer.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
 import './Footer.css'
-const Footer = props => {
+const Footer = () => {
     return (
         <div className="footer">
             <div className="footer__left"></div>
+            <div className="footer__center">
+                <ShuffleIcon className="footer__green" />
+                <SkipPreviousIcon  className="footer__icon" />
+                <PlayCircleOutlineIcon fontSize="large" className="footer__icon"/>
+                <SkipNextIcon  className="footer__icon" />
+                <RepeatIcon className="footer__green" />
+            </div>
+            <div className="footer__right"></div>
 
         </div>
     );
 };
 
-Footer.propTypes = {
-
-};
 
 export default Footer;
