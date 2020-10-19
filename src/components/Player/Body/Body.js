a3bfd3bb18757b4a5fd0115217e15ef7f0dc67a0
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-10-19 12:47:52 +0100

    added proptypes on body

--- src/components/Player/Body/Body.js
+++ src/components/Player/Body/Body.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import './Body.css'
+import PropTypes from 'prop-types';
 
-const Body = () => {
+const Body = props => {
     return (
         <div>
-            
+            i am the body
+
         </div>
     );
+};
+
+Body.propTypes = {
+    
 };
 
 export default Body;
