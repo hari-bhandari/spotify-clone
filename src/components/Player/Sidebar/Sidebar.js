cacf9622adfa6768bedbe23ca686cb759a627c85
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-10-19 12:38:28 +0100

    added sidebar

---
+++ src/components/Player/Sidebar/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Sidebar = props => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+Sidebar.propTypes = {
+    
+};
+
+export default Sidebar;
